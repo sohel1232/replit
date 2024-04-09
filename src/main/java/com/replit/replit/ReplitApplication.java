e7c7582bea5d41a312e1cb8272203573ad2efe81
sohel khan <m.sohelkhan2324516@gmail.com>
2024-04-09 14:42:03 +0530

    init spring

---
+++ src/main/java/com/replit/replit/ReplitApplication.java
@@ -0,0 +1,13 @@
+package com.replit.replit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/replit/replit/ReplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replit.replit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
